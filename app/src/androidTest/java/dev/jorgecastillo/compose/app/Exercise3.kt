95b5845590feb1b0fd6adb30badfa0ef0cbdb039
Jorge Castillo <jorge.castillo.prz@gmail.com>
2022-08-25 18:42:46 +0200

    Exercise 3 description.

--- app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise3.kt
+++ app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise3.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import dev.jorgecastillo.compose.app.ui.composables.SocialNetworkUser
+import dev.jorgecastillo.compose.app.ui.composables.SpeakersScreen
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
@@ -16,17 +17,88 @@ import org.junit.Test
 /**
  * ### Exercise 3 👩🏾‍💻
  *
- * This UI test runs the [SocialNetworkUser] Composable within an empty Activity and asserts over
- * the value of the displayed name, location, text of the button, and button click interaction.
+ * This exercise is a bit longer than the previous ones. We want to create a complete screen where
+ * we will show a scrollable list of Speakers. The screen to implement is [SpeakersScreen].
+ *
+ * Please use the exercise3.png image from the screenshots directory at the root of this project as
+ * a reference for this exercise.
  *
  * To complete this exercise:
  *
- * 1. Use the Text Composable twice, one for the name and another one for the location.
- * 2. Use the Column Composable to align the two texts vertically, the one below the other.
- * 3. Use a Row to align horizontally the two buttons (already aligned vertically) with a new Button
- *    that you must add. Add the "Follow" text to the Button.
- * 4. Add a click listener to the button that calls the provided callback for it.
- * 5. Run the test.
+ * 1. Use Scaffold at the root level. The Scaffold Composable gives you the material looks of an
+ *    average Android app, with parameters to add a TopAppBar, a FAB, a BottomBar, some content for
+ *    the screen and a few more things. It's pretty much like a template Composable to build an
+ *    average Android UI.
+ *
+ * 2. Add a TopAppBar to the Scaffold with the title "Speakers". You can use a normal Text
+ *    Composable for the title text.
+ *
+ * 3. Add a FloatingActionButton with an "add" material icon. You can use the Icon Composable for
+ *    this, and rememberVectorPainter to draw the icon on it. You can use Icons.Default.Add as the
+ *    icon vector. Feel free to leave the onClick lambda empty for now.
+ *
+ * 4. For the content lambda, use a Column where we will show the list of speakers. Remember to use
+ *    the provided PaddingValues argument from the content lambda to add it as padding
+ *    (i.e: Modifier.padding) to the Column at the root of the content. This is needed to ensure
+ *    that the correct offset is used to leave room for the potential TopAppBar and BottomAppBar.
+ *
+ * 5. Make the Column scrollable by applying a Modifier.verticalScroll(rememberScrollState()) to it.
+ *    We will understand states later in this course, so bear with me for now. You can chain this
+ *    new modifier with the previous one (padding) by using the dot notation:
+ *
+ *    Modifier
+ *     .padding(contentPadding)
+ *     .verticalScroll(rememberScrollState())
+ *
+ *    This is true for all modifiers, and I recommend you to do it from now on wherever you have
+ *    more than one modifier in a Composable.
+ *
+ * 6. Let's add some logic to our Composable now. Since Composables are plain Kotlin functions, we
+ *    can use any Kotlin goodies inside, like any of the collection apis. forEach {} speaker on the
+ *    list provided as a parameter, add a SpeakerCard to the column to draw the Speaker.
+ *
+ *    Time to implement the SpeakerCard.
+ *
+ * 7. Use the Card Composable for the SpeakerCard, and make it fill the max available width by
+ *    adding the Modifier.fillMaxWidth() to it. You can also add some padding to it like
+ *    R.dimen.spacing_small. To load resources in Compose, there are built in utility functions
+ *    you can use, like dimensionResource() in this case.
+ *
+ * 8. Inside the Card, let's add a Row with some padding like R.dimen.spacing_regular. This Row will
+ *    contain an Image Composable for the speaker avatar, and a Column with two texts for the name
+ *    and the company. To draw the image you'll need to pass a painterResource, and you can use the
+ *    provided function avatarResForId(speaker.id) in order to get the corresponding image resource.
+ *    These images are local drawables. Make the image contentScale be ContentScale.Crop so it fills
+ *    the Composable size completely without stretching. Set a size of
+ *    Modifier.size(dimensionResource(id = R.dimen.avatar_size)) to the Image and clip it as a
+ *    circle with Modifier.clip(CircleShape). (Note that there are built in shapes for different
+ *    shapes apart from Circle. Add the mentioned Column after the Image, and set some
+ *    padding to it, e.g: R.dimen.spacing_regular.
+ *
+ * 9. Finally, add two Text Composables inside the Column with the material styles h6 and caption.
+ *    You can do it via the style parameter like: MaterialTheme.typography.h6. This will grab the
+ *    text style from the material theme we have already applied to the app. Show the texts
+ *    speaker.name and speaker.company on them. We will learn material and theming in Compose
+ *    in-depth later in this course.
+ *
+ * 10. There is a preview that you can use for all this work to double check it looks the way it
+ *     needs. On top of that, you can check the screenshot called exercise3.png from the screenshots
+ *     directory at the root folder, or even set the SpeakersScreen Composable as the content of the
+ *     MainActivity in order to run the app in an emulator like:
+ *
+ *     override fun onCreate(savedInstanceState: Bundle?) {
+ *       super.onCreate(savedInstanceState)
+ *       setContent {
+ *         val speakersRepository = FakeSpeakerRepository()
+ *         ComposeAndInternalsTheme {
+ *           SpeakersScreen(speakers = speakersRepository.getSpeakers())
+ *         }
+ *       }
+*      }
+ *
+ *    The fake repository will provide the speakers we need.
+ *
+ * 11. Run the test.
  */
 class Exercise3Test {
 
