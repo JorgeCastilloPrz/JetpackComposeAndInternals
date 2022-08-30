401222cbce88c77acf9d66362ad6b73327b126c2
Jorge Castillo <jorge.castillo.prz@gmail.com>
2022-08-30 15:17:01 +0200

    Updates.

--- app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
@@ -3,13 +3,6 @@ package dev.jorgecastillo.compose.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import dev.jorgecastillo.compose.app.data.FakeSpeakerRepository
 import dev.jorgecastillo.compose.app.ui.composables.LazySpeakersScreen
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
@@ -20,12 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val speakersRepository = FakeSpeakerRepository()
             ComposeAndInternalsTheme {
-//                LazySpeakersScreen(speakers = speakersRepository.getSpeakers())
-                Column(modifier = Modifier.padding(16.dp).size(72.dp)) {
-                    Text("Text 1")
-                    Text("Text 2")
-                    Text("Text 2")
-                }
+                LazySpeakersScreen(speakers = speakersRepository.getSpeakers())
             }
         }
     }
