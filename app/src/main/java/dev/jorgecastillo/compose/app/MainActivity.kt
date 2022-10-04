3e8a8c5b69eb770d9d7783f3fa076662e1f4f1bd
Jorge Castillo <jorge.castillo.prz@gmail.com>
2022-10-04 16:12:00 +0200

    Up to date.

--- app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import dev.jorgecastillo.compose.app.data.FakeSpeakerRepository
-import dev.jorgecastillo.compose.app.ui.composables.MainScreen
+import dev.jorgecastillo.compose.app.ui.composables.LazySpeakersScreen
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 import kotlin.time.ExperimentalTime
 
@@ -17,8 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val speakersRepository = FakeSpeakerRepository()
             ComposeAndInternalsTheme {
-//                LazySpeakersScreen(speakers = speakersRepository.getSpeakers())
-                MainScreen()
+                LazySpeakersScreen(speakers = speakersRepository.getSpeakers())
             }
         }
     }
