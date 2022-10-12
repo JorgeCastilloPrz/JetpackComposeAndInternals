18af117e23bb33e9efc699ca635f13f5f6647e99
Jorge Castillo <jorge.castillo.prz@gmail.com>
2022-10-12 11:05:44 +0200

    Up to date.

--- app/src/androidTest/java/dev/jorgecastillo/compose/app/Theme.kt
+++ app/src/androidTest/java/dev/jorgecastillo/compose/app/Theme.kt
@@ -22,20 +22,6 @@ private val LightColors = lightColors(
     secondary = Teal200
 )
 
-private val DarkPinkColors = darkColors(
-    primary = Pink200,
-    primaryVariant = Pink700,
-    secondary = Teal200,
-    surface = Pink200
-)
-
-private val LightPinkColors = lightColors(
-    primary = Pink500,
-    primaryVariant = Pink700,
-    secondary = Teal200,
-    surface = Pink700
-)
-
 @Composable
 fun MyAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -45,25 +31,6 @@ fun MyAppTheme(
         DarkColors
     } else {
         LightColors
-    }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
-}
-
-@Composable
-fun PinkTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        DarkPinkColors
-    } else {
-        LightPinkColors
     }
 
     MaterialTheme(
