bac6225fd790afdb8e0f869f95f2f7f6c2e2679a
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2023-02-09 15:02:13 +0100

    Up to date.

--- app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise2.kt
+++ app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise2.kt
@@ -1,16 +1,7 @@
 package dev.jorgecastillo.compose.app
 
-import androidx.activity.ComponentActivity
-import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onParent
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.printToLog
-import dev.jorgecastillo.compose.app.ui.composables.NamePlate
 import dev.jorgecastillo.compose.app.ui.composables.SocialNetworkUser
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 import org.hamcrest.CoreMatchers.`is`
@@ -23,6 +14,9 @@ import org.junit.Test
  *
  * This UI test runs the [SocialNetworkUser] Composable within an empty Activity and asserts over
  * the value of the displayed name, location, text of the button, and button click interaction.
+ *
+ * Use the screenshots/exercise2.png screenshot as a reference for how the Composable must look in
+ * the end.
  *
  * To complete this exercise:
  *
