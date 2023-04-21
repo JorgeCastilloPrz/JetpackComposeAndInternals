53502502f3423ca21962e8ce938f0cf4c1cfdb19
Jorge Castillo <jorge.castillo.prz@gmail.com>
2023-04-21 14:49:25 +0200

    adds exercise 8 description

--- app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise8.kt
+++ app/src/androidTest/java/dev/jorgecastillo/compose/app/Exercise8.kt
@@ -1,29 +1,123 @@
 package dev.jorgecastillo.compose.app
 
+import androidx.compose.material.Card
+import androidx.compose.runtime.CompositionLocal
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performScrollToNode
+import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import dev.jorgecastillo.compose.app.data.FakeSpeakerRepository
+import dev.jorgecastillo.compose.app.ui.composables.LazySpeakersScreen
+import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
+import org.junit.Rule
+import org.junit.Test
+
 /**
  * ### Exercise 8 👩🏾‍💻
  *
- * In this exercise we must create a Composable that uses BoxWithConstraints to display different
- * UIs for phone and tablet.
+ * This exercise introduces [CompositionLocal], the two different ways to create a CompositionLocal,
+ * and the difference between the two.
  *
- * Please use the exercise7_phone.png and exercise7_tablet.png images from the screenshots directory
- * at the root of this project as a reference for this exercise. That is how phone and tablet UIs
- * are expected to look correspondingly.
+ * There are two ways to create a CompositionLocal:
  *
- * Very recommended!! Use a Pixel C (tablet) emulator for running this test, since we need to
- * emulate both phone and tablet layouts on the same device, so we'll need enough space available
- * for both.
+ * 1. [compositionLocalOf]:
  *
- * To complete this exercise, we must use BoxWithConstraints to implement [AdaptativeScreen] so:
+ *    When updated, invalidates only the Composables reading its current value. Use it for cases
+ *    where the value is expected to change over time, since it is more performant in those
+ *    scenarios than the alternative below 👇
  *
- * 1. For tablets (maxWidth >= 600.dp) it shows a Row with the [ProfileScreen] with width 320.dp and
- *    max height on the left, and [FriendsScreen] taking the rest of the screen width to the right
- *    (you can use Modifier.weight(1f) for that).
+ * 2. [staticCompositionLocalOf]:
  *
- * 2. For phones (maxWidth < 600.dp) it shows the [ProfileScreen] only, filling all the available
- *    screen space (Modifier.fillMaxSize()).
+ *    Reads to this object are not tracked by Compose, so when updated, it invalidates the entire
+ *    `content` lambda where the CompositionLocal is provided. Use this one for cases where the
+ *    value provided will likely not change ever, since it will be more performant for those.
  *
- * 3. Run the test.
+ * Now, time for the exercise.
+ *
+ * Imagine that we have a custom design system where we need to provide some custom elevation value
+ * for all the elevated components like [Card]s.
+ *
+ * There are two tests on this test class. In both of them, we must provide the elevation via a
+ * [CompositionLocal] so it is exposed to the whole subtree, and then read from its current value to
+ * set the elevation of a nested [Card] Composable. The elevation will be defined via the
+ * [Elevations] data class.
+ *
+ * To complete this exercise:
+ *
+ * 1. Provide the elevation using a static [CompositionLocal] in the first test. Read from it to set
+ *    the [Card] elevation.
+ * 2. Run the test, it should verify that all Composables are recomposed once the value is updated.
+ *
+ * 3. Provide the elevation using a non-static [CompositionLocal] in the second test. Read from it
+ *    to set the [Card] elevation.
+ * 4. Run the test, it should verify that only the Composables reading from the value are
+ *    recomposed.
  */
 class Exercise8 {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    data class Elevations(val card: Dp = 0.dp, val default: Dp = 0.dp)
+
+    @Test
+    fun static_composition_local_does_not_trigger_granular_recomposition() {
+        // Start the app
+        composeTestRule.setContent {
+            ComposeAndInternalsTheme {
+                LazySpeakersScreen(speakers = FakeSpeakerRepository().getSpeakers().take(7))
+            }
+        }
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("John Doe"))
+        composeTestRule.onNodeWithText("John Doe").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Uber").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("Sylvia Lotte"))
+        composeTestRule.onNodeWithText("Sylvia Lotte").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Lyft").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("Apis Anoubis"))
+        composeTestRule.onNodeWithText("Apis Anoubis").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Twitter").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("Aeolus Phrixos"))
+        composeTestRule.onNodeWithText("Aeolus Phrixos").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Meta").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("Oz David"))
+        composeTestRule.onNodeWithText("Oz David").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList")
+            .performScrollToNode(hasText("Jagoda Viktorija"))
+        composeTestRule.onNodeWithText("Jagoda Viktorija").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Google").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+
+        composeTestRule.onNodeWithTag("SpeakersList").performScrollToNode(hasText("Dympna Bride"))
+        composeTestRule.onNodeWithText("Dympna Bride").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Snapchat").assertIsDisplayed()
+
+        composeTestRule.onRoot().printToLog("Exercise 4")
+    }
 }
